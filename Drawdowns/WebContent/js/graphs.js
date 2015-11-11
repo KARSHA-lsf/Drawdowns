0c921dd8e171e4f3b9f2bf09c324eae5641bec15
tharindu <tharindu@tharindu-PC>
2015-11-11 13:56:42 -0800

    graph js updated with index grph changes

--- Drawdowns/WebContent/js/graphs.js
+++ Drawdowns/WebContent/js/graphs.js
@@ -126,27 +126,13 @@ function drawIndex(json_ary){
 	    	
 	        y:{
 	        	max:0.1,
-<<<<<<< HEAD
+
                 min:-0.4,
 	            padding : 0,
 	            tick:{
 	            	//count:10,
 	            },
-=======
-<<<<<<< HEAD
-	            min:-0.3,
-	            label: 'Drawdown value',
-                tick: {
-	              values: [0.10, 0.05, 0, ,-0.05, -0.10, -0.15, -0.20, -0.25, -0.30]
-	            }
-=======
-                min:-0.3,
-                label: 'Drawdown value',
-                tick: {
-                    values: [0.10, 0.05, 0, ,-0.05, -0.10, -0.15, -0.20, -0.25, -0.30]
-                  }
->>>>>>> branch 'master' of https://github.com/Karsha-Project-LSF/Drawdowns.git
->>>>>>> branch 'master' of https://github.com/Karsha-Project-LSF/Drawdowns.git
+
 	            //max : 2300,
 	        },
 	    },
